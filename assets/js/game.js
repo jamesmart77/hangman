8efe9d1401f54318fa6a85b328173a56cc888919
James Martineau <James.Martineau@libertymutual.com>
2017-10-06 15:33:02 -0400

    hangman pic added, sidebar working, footer laid

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 needs to be at top for scope */
 var guessWordsArr = ["Telescope", "Martian", "Uranus", "Galaxy", "Asteroid", "Spock", "Stellar", "Nebulous", "Cryogenesis", "Vacuum"];
 
-var totalGuesses = 12;
+var totalGuesses = 10;
 
 var guess = {
     previousLetters: [],
@@ -42,12 +42,16 @@ function wordLoad() {
 
     let resultString = " _".repeat(challengeWordDigits); //build placeholders for challenge word
     challengeWord.textContent = resultString; //overwrite existing text
-
-    btn.style.visibility = 'hidden';
 }
 
 //user guess
 document.onkeyup = function (event) {
+
+    var guessInput = document.getElementById("fullGuess");
+    //if user is typing full guess, skip everything
+    if (document.activeElement === guessInput) {
+        return //skip everything...user is typing full guess
+    }
 
     //get letter typed and make lowercase
     var letter = String.fromCharCode(event.keyCode).toLowerCase();
@@ -80,7 +84,7 @@ function charChecker(letter) {
 // ============================================ //
 //add new letter to previous letter array if not already included
 function pushLetter(letter) {
-    
+
     var doesNotExist = true; //set flag to assume it's a new letter
 
     //Check to see if letter was already entered
@@ -93,14 +97,14 @@ function pushLetter(letter) {
 
     //if a new entry
     if (doesNotExist) {
-        guess.previousLetters.push(letter);//add to guess list
-        guess.remainingGuess -= 1;//subtract remaining guesses
+        guess.previousLetters.push(letter); //add to guess list
+        // guess.remainingGuess -= 1; //subtract remaining guesses
 
         console.log("remaining guess " + guess.remainingGuess);
         console.log("previous letters " + guess.previousLetters);
         console.log("current word " + guessWordsArr[playerStats.currentWord]);
 
-        isEntryInWord(letter);//check if guess is in challenge word
+        isEntryInWord(letter); //check if guess is in challenge word
         didPlayerWin();
         outOfGuesses();
         updateStats();
@@ -115,19 +119,21 @@ function pushLetter(letter) {
 //searching for player guess in challenge word
 function isEntryInWord(letter) {
     var challengeWordText = challengeWord.textContent;
-
     var wordText = guessWordsArr[playerStats.currentWord].toLowerCase();
+    var isLetterFound = false; //set flag to see if letter is found
 
     //loop through each letter of challenge word and compare
-    for (var i = 0; i <= wordText.length-1; i++) {
+    for (var i = 0; i <= wordText.length - 1; i++) {
 
         //if match is found, replace corresponding underscore placeholder
         if (letter === wordText.charAt(i)) {
-            
+
+            isLetterFound = true;
+
             //*2 to to map raw challenge word to challenge word with spaces between each underscore
-            var beginningPart = challengeWordText.slice(0,i*2);//grab letters up to i*2
-            var endPart = challengeWordText.slice(i*2+2);//grab letters after i*2 thru end -- + 2 is to skip over found letter
-            
+            var beginningPart = challengeWordText.slice(0, i * 2); //grab letters up to i*2
+            var endPart = challengeWordText.slice(i * 2 + 2); //grab letters after i*2 thru end -- + 2 is to skip over found letter
+
             //overwrite previous challenge word to include letters found
             //concatenate space to not impact string length for future calculations
             challengeWordText = beginningPart + " " + letter + endPart;
@@ -136,25 +142,57 @@ function isEntryInWord(letter) {
     //update challenge word with letters found
     challengeWord.textContent = challengeWordText;
 
+    if (!isLetterFound) {
+        guess.remainingGuess -= 1;
+        updateHangmanPicture();
+    }
+
 }
 
 // ============================================ //
 
-function didPlayerWin(){
+function updateHangmanPicture() {
+    $(".hangman-pic").empty();//clear div for new photo
+
+    let imgHangman = $("<img>");
+    let hangmanPicPath = "assets/images/" + guess.remainingGuess + ".png";
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imgHangman.addClass("hangman-img");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    imgHangman.attr("src", hangmanPicPath);
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    //imgHangman.attr("data-crystalvalue", numberOptions[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $(".hangman-pic").append(imgHangman);
+}
+
+// ============================================ //
+
+function didPlayerWin() {
     var remainingChallengeText = challengeWord.textContent;
 
-    if(remainingChallengeText.indexOf('_') > -1) {
+    if (remainingChallengeText.indexOf('_') > -1) {
         //player has not won yet
     } else {
         //get accomplished challenge word before it updates
-        var wordName = guessWordsArr[playerStats.currentWord];
-        playerStats.wins += 1;
-        gameReset();
-
-        //update modal for recent win
-        modalText.innerHTML = "<p>Nice Job! You figured out the word was <b>" + wordName + "</b>!</p>"
-        modal.style.display = "block";//display modal    
+        victory();
     }
+}
+
+function victory() {
+    var wordName = guessWordsArr[playerStats.currentWord];
+    playerStats.wins += 1;
+    gameReset();
+
+    //update modal for recent win
+    modalText.innerHTML = "<p>Nice Job! You figured out the word was <b>" + wordName + "</b>!</p>"
+    modal.style.display = "block"; //display modal
 }
 
 // ============================================ //
@@ -177,25 +215,39 @@ function updateStats() {
 }
 
 // ============================================ //
-function outOfGuesses(){
+function outOfGuesses() {
     if (guess.remainingGuess === 0) {
         alert("Ahh shucks! You didn't get the word...\n\nBetter luck on the next word!");
         gameReset();
     }
 }
+
 function gameReset() {
-    
-        if (playerStats.currentWord === guessWordsArr.length - 1) { //if player is on last word
-            playerStats.currentWord = 0;//start with first word
-        } else {
-            playerStats.currentWord += 1;//else go to next word
-        }
 
-        wordLoad();//load next word
+    if (playerStats.currentWord === guessWordsArr.length - 1) { //if player is on last word
+        playerStats.currentWord = 0; //start with first word
+    } else {
+        playerStats.currentWord += 1; //else go to next word
+    }
 
-        //clear previous guesses
-        guess.previousLetters = [];
-        guess.remainingGuess = totalGuesses;
+    wordLoad(); //load next word
+
+    //clear previous guesses
+    guess.previousLetters = [];
+    guess.remainingGuess = totalGuesses;
 }
 
+function checkGuess() {
+    // var guessInput = document.getElementById("fullGuess");
+    var fullGuess = document.getElementById("fullGuess").value;
+
+    if (fullGuess.toLowerCase() === guessWordsArr[playerStats.currentWord].toLowerCase()) {
+        victory();
+    } else {
+        guess.remainingGuess -= 1;
+        guessInput.textContent = "";
+        outOfGuesses();
+    }
+    updateStats();
+}
 // }
